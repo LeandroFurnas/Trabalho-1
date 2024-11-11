ed76b25768166bd3ca7ce5353a3108cde16a0f1e
LeandroFurnas <furnas2008@gmail.com>
2024-11-11 00:11:16 -0100

    Create Trabalho 2.cpp

---
+++ Trabalho 2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+float pi = 3.1416;
+float raio;
+
+
+    cout << "idique o valor do raio: ";
+    cin >> raio;
+    cout << "O perimetro da circunferência  é: ";
+    cout << (2 * pi) * raio << endl;
+    cout << "A área da circunferência é: "; 
+    cout << pi * (raio*raio)<< endl;
+
+
+    return 0;
+}
+
